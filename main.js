f9b0fd0ee34e897a5c107e1d02b0f78c9f0493dc
CatAvadani <catalinaavadani@gmail.com>
2023-11-06 11:14:17 +0100

    show product title on the screen

--- main.js
+++ main.js
@@ -4,5 +4,13 @@ function main() {
   renderProducts();
 }
 function renderProducts() {
-  console.log(products);
+  // adding the element in html
+  const main = document.querySelector("#product-container");
+  for (const product of products) {
+    // this creates the element in js
+    const card = document.createElement("div");
+    card.textContent = product.title;
+
+    main.appendChild(card);
+  }
 }
