93f543906f1ad2d120f4e65a2d9cc9124b2d6d6c
CatAvadani <catalinaavadani@gmail.com>
2023-11-06 10:29:37 +0100

    Initial commit with the base code structure

---
+++ data.js
@@ -0,0 +1,17 @@
+const products = [
+  {
+    id: 1,
+    title: "One",
+    price: 45,
+  },
+  {
+    id: 2,
+    title: "Götebors Rape",
+    price: 50,
+  },
+  {
+    id: 3,
+    title: "General",
+    price: 65,
+  },
+];
